fbc4b96625902867ac89b8dec005a35971c6b1ac
Shanay Murdock <murdock.shanay@gmail.com>
2019-02-10 17:58:34 -0600

    Added initial starter files for project 1

---
+++ SoccerCoordinator/SoccerCoordinator/Soccer_coordinator.swift
@@ -0,0 +1,11 @@
+//
+//  Soccer_coordinator.swift
+//  SoccerCoordinator
+//
+//  Created by Shanay Murdock on 2/10/19.
+//  Copyright © 2019 Treehouse. All rights reserved.
+//
+
+import Foundation
+
+

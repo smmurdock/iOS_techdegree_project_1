b279b7e7792d0a9e96d8c09979728389a5e3250c
Shanay Murdock <murdock.shanay@gmail.com>
2019-02-10 19:14:08 -0600

    Removed duplicated data

--- SoccerCoordinator/SoccerCoordinator/Soccer_coordinator.swift
+++ SoccerCoordinator/SoccerCoordinator/Soccer_coordinator.swift
@@ -9,17 +9,6 @@
 import Foundation
 
 
-//
-//  Soccer_coordinator.swift
-//  SoccerCoordinator
-//
-//  Created by Shanay Murdock on 2/10/19.
-//  Copyright © 2019 Treehouse. All rights reserved.
-//
-
-import Foundation
-
-
 /*
  ---------------------
  Player Data

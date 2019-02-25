c27fd191b9f6a01e3ea6f3575bf7102e8dd15d04
Shanay Murdock <murdock.shanay@gmail.com>
2019-02-25 13:23:38 -0600

    Update to player dictionary
    Updates to collections for player experience and teams

--- SoccerCoordinator/SoccerCoordinator/Soccer_coordinator.swift
+++ SoccerCoordinator/SoccerCoordinator/Soccer_coordinator.swift
@@ -22,148 +22,53 @@ let players: [[String: Any]] = []
 
 // Dictionary of Players
 
-let player1: [String: Any] =
-    [
-        "playerName": "Joe Smith",
-        "heightInInches": 42.0,
-        "hasSoccerExperience": true,
-        "guardianNames": "Jim and Jan Smith"
-    ]
-
-let player2: [String: Any] =
-    [
-        "playerName": "Jill Tanner",
-        "heightInInches": 36.0,
-        "hasSoccerExperience": true,
-        "guardianNames": "Clara Tanner"
-    ]
-
-let player3: [String: Any] =
-    [
-        "playerName": "Bill Bonn",
-        "heightInInches": 43.0,
-        "hasSoccerExperience": true,
-        "guardianNames": "Sara and Jenny Bonn"
-    ]
-
-let player4: [String: Any] =
-    [
-        "playerName": "Eva Gordon",
-        "heightInInches": 45.0,
-        "hasSoccerExperience": false,
-        "guardianNames": "Wendy and Mike Gordon"
-    ]
-
-let player5: [String: Any] =
-    [
-        "playerName": "Matt Gil",
-        "heightInInches": 40.0,
-        "hasSoccerExperience": false,
-        "guardianNames": "Charles and Sylvia Gill"
-    ]
-
-let player6: [String: Any] =
-    [
-        "playerName": "Kimmy Stein",
-        "heightInInches": 41.0,
-        "hasSoccerExperience": false,
-        "guardianNames": "Bill and Hillary Stein"
-    ]
-
-let player7: [String: Any] =
-    [
-        "playerName": "Sammy Adams",
-        "heightInInches": 45.0,
-        "hasSoccerExperience": false,
-        "guardianNames": "Jeff Adams"
-    ]
-
-let player8: [String: Any] =
-    [
-        "playerName": "Karl Saygan",
-        "heightInInches": 42.0,
-        "hasSoccerExperience": true,
-        "guardianNames": "Heather Bledsoe"
-    ]
-
-let player9: [String: Any] =
-    [
-        "playerName": "Suzane Greenberg",
-        "heightInInches": 44.0,
-        "hasSoccerExperience": true,
-        "guardianNames": "Henrietta Dumas"
-    ]
-
-let player10: [String: Any] =
-    [
-        "playerName": "Sal Dali",
-        "heightInInches": 41.0,
-        "hasSoccerExperience": false,
-        "guardianNames": "Gala Dali"
-    ]
-
-let player11: [String: Any] =
-    [
-        "playerName": "Joe Kavalier",
-        "heightInInches": 39.0,
-        "hasSoccerExperience": false,
-        "guardianNames": "Sam and Elaine Kavalier"
-    ]
-
-let player12: [String: Any] =
-    [
-        "playerName": "Ben Finkelstein",
-        "heightInInches": 44.0,
-        "hasSoccerExperience": false,
-        "guardianNames": "Aaron and Jill Finkelstein"
-    ]
-
-let player13: [String: Any] =
-    [
-        "playerName": "Diego Soto",
-        "heightInInches": 41.0,
-        "hasSoccerExperience": true,
-        "guardianNames": "Robin and Sarika Soto"
-    ]
-
-let player14: [String: Any] =
-    [
-        "playerName": "Chloe Alaska",
-        "heightInInches": 47.0,
-        "hasSoccerExperience": false,
-        "guardianNames": "David and Jamie Alaska"
-    ]
-
-let player15: [String: Any] =
-    [
-        "playerName": "Arnold Willis",
-        "heightInInches": 43.0,
-        "hasSoccerExperience": false,
-        "guardianNames": "Claire Willis"
-    ]
-
-let player16: [String: Any] =
-    [
-        "playerName": "Phillip Helm",
-        "heightInInches": 44.0,
-        "hasSoccerExperience": true,
-        "guardianNames": "Thomas Helm and Eva Jones"
-    ]
-
-let player17: [String: Any] =
-    [
-        "playerName": "Les Clay",
-        "heightInInches": 42.0,
-        "hasSoccerExperience": true,
-        "guardianNames": "Wynonna Brown"
-    ]
-
-let player18: [String: Any] =
-    [
-        "playerName": "Herschel Krustofski",
-        "heightInInches": 45.0,
-        "hasSoccerExperience": true,
-        "guardianNames": "Hyman and Rachel Krustofski"
-    ]
+let player1: [String: Any] = ["playerName": "Joe Smith", "heightInInches": 42.0, "hasSoccerExperience": true, "guardianNames": "Jim and Jan Smith"]
+let player2: [String: Any] = ["playerName": "Jill Tanner", "heightInInches": 36.0, "hasSoccerExperience": true,"guardianNames": "Clara Tanner"]
+let player3: [String: Any] = ["playerName": "Bill Bonn", "heightInInches": 43.0, "hasSoccerExperience": true, "guardianNames": "Sara and Jenny Bonn"]
+let player4: [String: Any] = ["playerName": "Eva Gordon", "heightInInches": 45.0, "hasSoccerExperience": false, "guardianNames": "Wendy and Mike Gordon"]
+let player5: [String: Any] = ["playerName": "Matt Gil", "heightInInches": 40.0, "hasSoccerExperience": false, "guardianNames": "Charles and Sylvia Gill"]
+let player6: [String: Any] = ["playerName": "Kimmy Stein", "heightInInches": 41.0, "hasSoccerExperience": false,"guardianNames": "Bill and Hillary Stein"]
+let player7: [String: Any] = ["playerName": "Sammy Adams", "heightInInches": 45.0, "hasSoccerExperience": false, "guardianNames": "Jeff Adams"]
+let player8: [String: Any] = ["playerName": "Karl Saygan", "heightInInches": 42.0,"hasSoccerExperience": true, "guardianNames": "Heather Bledsoe"]
+let player9: [String: Any] = ["playerName": "Suzane Greenberg", "heightInInches": 44.0, "hasSoccerExperience": true, "guardianNames": "Henrietta Dumas"]
+let player10: [String: Any] = ["playerName": "Sal Dali", "heightInInches": 41.0, "hasSoccerExperience": false, "guardianNames": "Gala Dali"]
+let player11: [String: Any] = ["playerName": "Joe Kavalier","heightInInches": 39.0, "hasSoccerExperience": false, "guardianNames": "Sam and Elaine Kavalier"]
+let player12: [String: Any] = ["playerName": "Ben Finkelstein", "heightInInches": 44.0, "hasSoccerExperience": false, "guardianNames": "Aaron and Jill Finkelstein"]
+let player13: [String: Any] = ["playerName": "Diego Soto", "heightInInches": 41.0, "hasSoccerExperience": true, "guardianNames": "Robin and Sarika Soto"]
+let player14: [String: Any] = ["playerName": "Chloe Alaska", "heightInInches": 47.0, "hasSoccerExperience": false, "guardianNames": "David and Jamie Alaska"]
+let player15: [String: Any] = ["playerName": "Arnold Willis", "heightInInches": 43.0, "hasSoccerExperience": false, "guardianNames": "Claire Willis"]
+let player16: [String: Any] = ["playerName": "Phillip Helm", "heightInInches": 44.0, "hasSoccerExperience": true, "guardianNames": "Thomas Helm and Eva Jones"]
+let player17: [String: Any] = ["playerName": "Les Clay", "heightInInches": 42.0, "hasSoccerExperience": true, "guardianNames": "Wynonna Brown"]
+let player18: [String: Any] = ["playerName": "Herschel Krustofski", "heightInInches": 45.0, "hasSoccerExperience": true, "guardianNames": "Hyman and Rachel Krustofski"]
 
 var allLeaguePlayers = [player1, player2, player3, player4, player5, player6, player7, player8, player9, player10, player11, player12, player13, player14, player15, player16, player17, player18]
+
+// Empty collections to allow separation of experienced and non-experienced players
+
+var experiencedPlayers: [[String: Any]] = []
+var inexperiencedPlayers: [[String: Any]] = []
+
+// Empty collections for team names to begin sorting
+
+var teamSharks: [[String: Any]] = []
+var teamDragons: [[String: Any]] = []
+var teamRaptors: [[String: Any]] = []
+
+
+// Total number of league players
+
+var fullSoccerLeague = [teamSharks, teamDragons, teamRaptors]
+
+
+// Logic to separate experienced and non-experienced players evenly
+
+for player in allPlayers {
+    if let isPlayerExperienced = player
+}
+
+
+/*
+ ---------------------
+ Letter to Parents
+ ---------------------
+*/
